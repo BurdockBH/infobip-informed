ae87c4a324129d22f0f3f1cbaeb487346e07353c
BurdockBH <77117724+BurdockBH@users.noreply.github.com>
2022-12-08 15:39:49 +0100

    Added article pages (#6)
    * Added article pages
    
    * Added article pages
    Moved styles to main.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+import "./main.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./Pages/LandingPage";
 import ArticlePage from "./Pages/ArticlePage";
--- src/Pages/HeadArticlePage.tsx
+++ src/Pages/HeadArticlePage.tsx
@@ -3,13 +3,41 @@ import MainNavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 
 import Typography from "@mui/material/Typography";
-import { DUMMY_HEADER } from "../const/const";
+import { DUMMY_TEXT } from "../const/const";
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
 
 function HeadArticlePage() {
   return (
     <div className="App">
       <MainNavBar />
-      <Typography>{DUMMY_HEADER.description}</Typography>
+      <Card>
+        <div>
+          <CardMedia
+            height="350"
+            component="img"
+            image={
+              "https://www.bug.hr/img/domaci-infobip-sada-vrijedi-vise-od-milijardu-dolara_hqrVf3.jpg"
+            }
+            alt="News"
+            sx={{
+              "object-position": "top",
+            }}
+          />
+          <div className="article-title">
+            <Typography variant="h1">This just in</Typography>
+          </div>
+        </div>
+      </Card>
+      <br />
+      <Typography width="70%" sx={{ margin: "auto" }}>
+        {DUMMY_TEXT + DUMMY_TEXT}
+        <br />
+        <br />
+        {DUMMY_TEXT + DUMMY_TEXT}
+      </Typography>
+      <br />
+      <br />
       <Footer />
     </div>
   );
