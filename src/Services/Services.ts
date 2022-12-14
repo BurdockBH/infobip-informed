caaf23c58122acd87c33b9a7260888cf913dedd2
BurdockBH <77117724+BurdockBH@users.noreply.github.com>
2022-12-14 15:07:59 +0100

    Separated comments and articles end-points. (#14)
    * Separated comments and articles end-points.
    Comments are now fetched inside their own pages.
    
    * Separated comments and articles end-points.
    Comments are now fetched inside their own pages.
    
    * Separated comments and articles end-points.
    Comments are now fetched inside their own pages.
    Changed title icon and title.

--- src/Services/Services.ts
+++ src/Services/Services.ts
@@ -4,6 +4,10 @@ export const getArticlesData = (controller: any) => {
   return axios.get('http://localhost:8000/articles', { signal: controller.signal });
 };
 
+export const getCommentData = (controller: any, id: any) => {
+  return axios.get(`http://localhost:8000/comments?id=${id}`, { signal: controller.signal });
+};
+
 export const postComment = (comment: any) => {
   return axios.post('http://localhost:8000/new-comment', comment);
 };
