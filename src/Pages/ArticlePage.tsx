86d10f99ba169d0ad3a87805394a04a43df0e814
Edo <edocicak@gmail.com>
2022-12-15 14:25:20 +0100

    Added share article buttons. Added show/hide comments button

--- src/Pages/ArticlePage.tsx
+++ src/Pages/ArticlePage.tsx
@@ -7,11 +7,22 @@ import Card from '@mui/material/Card';
 import Typography from '@mui/material/Typography';
 import Comments from '../components/Comments';
 import { getCommentData, postComment } from '../Services/Services';
+import {
+  FacebookShareButton,
+  TwitterShareButton,
+  EmailShareButton,
+  FacebookIcon,
+  EmailIcon,
+  TwitterIcon,
+} from 'react-share';
+import { Button } from '@mui/material';
+import Box from '@mui/material/Box';
 
 function ArticlePage({ article }: any) {
   const { id } = useParams();
   const content = article.find((item: any) => item.id.toString() == id);
   const [comments, setComments] = useState<any[]>([]);
+  const [showComments, setShowComments] = useState({ status: 'Show Comments', display: 'none' });
 
   const getComments = useCallback((controller: any) => {
     getCommentData(controller, id).then((response) => setComments(response.data));
@@ -37,6 +48,13 @@ function ArticlePage({ article }: any) {
     }
   };
 
+  const handleShowComments = () => {
+    const commentShow = showComments.status == 'Show Comments' ? 'Hide Comments' : 'Show Comments';
+    const commentDisplay = showComments.display == 'flex' ? 'none' : 'flex';
+
+    setShowComments({ status: commentShow, display: commentDisplay });
+  };
+
   if (!content) return null;
   return (
     <div className='App'>
@@ -58,12 +76,35 @@ function ArticlePage({ article }: any) {
         </div>
       </Card>
       <br />
+      <div className='share-buttons'>
+        <FacebookShareButton url={`http://localhost:3000/article/${id}`} title={'titles'}>
+          <FacebookIcon size={50} round />
+        </FacebookShareButton>
+        <EmailShareButton url={`http://localhost:3000/article/${id}`} title={'titles'}>
+          <EmailIcon size={50} round />
+        </EmailShareButton>
+        <TwitterShareButton url={`http://localhost:3000/article/${id}`} title={'titles'}>
+          <TwitterIcon size={50} round />
+        </TwitterShareButton>
+      </div>
+      <br />
       <Typography width='75%' sx={{ margin: 'auto', 'font-size': '18px', 'text-align': 'justify' }}>
         {content?.text}
       </Typography>
       <br />
+
       <br />
-      <Comments handleComments={handleComments} comments={comments} />
+      <br />
+      <Box textAlign='center' sx={{ 'margin-bottom': '5%' }}>
+        <Button variant='contained' onClick={() => handleShowComments()}>
+          {showComments.status}
+        </Button>
+      </Box>
+      <Comments
+        display={showComments.display}
+        handleComments={handleComments}
+        comments={comments}
+      />
       <br />
       <br />
       <Footer />
