9d6b29860e49db15a480866628fe7bd4cec6a282
Edo <edocicak@gmail.com>
2022-12-15 10:27:49 +0100

    Added contact form Added email each input field validation. Added dialog pop-up.

--- src/Pages/ContactPage.tsx
+++ src/Pages/ContactPage.tsx
@@ -3,9 +3,15 @@ import MainNavBar from '../components/NavBar';
 import Footer from '../components/Footer';
 import { Button, TextField } from '@mui/material';
 import emailjs from '@emailjs/browser';
+import AlertDialog from '../components/Dialog';
 
 function Contact() {
   const [formData, setFormData] = useState({ name: '', email: '', message: '' });
+  const [emailError, setEmailError] = useState({ status: false, message: '' });
+  const [nameError, setNameError] = useState({ status: false, message: '' });
+  const [messageError, setMessageError] = useState({ status: false, message: '' });
+
+  const [open, setOpen] = React.useState(false);
 
   const sendEmail = () => {
     emailjs.send('service_qrbh14g', 'template_c0k8lg6', formData, 'Vnvo3cIQ2XdfDEPob').then(
@@ -18,41 +24,71 @@ function Contact() {
     );
   };
 
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const isValidEmail = (email: any) => {
+    return /\S+@\S+\.\S+/.test(email);
+  };
+
   return (
     <div className='App'>
       <MainNavBar />
       <div className='contact-form'>
         <TextField
-          sx={{ width: '50%', margin: 'auto' }}
+          FormHelperTextProps={{ style: { backgroundColor: 'rgb(238,238,238)', margin: '0' } }}
+          sx={{ background: 'white', width: '50%', margin: 'auto' }}
           id='outlined-basic'
           label='Name'
           multiline={true}
           value={formData.name}
+          error={nameError.status}
+          helperText={nameError.message}
           onChange={(event) => {
+            if (event.target.value === '')
+              setNameError({ status: true, message: 'Name cannot be empty' });
+            else setNameError({ status: false, message: '' });
             setFormData({ ...formData, name: event.target.value });
           }}
         />
         <br />
         <br />
         <TextField
-          sx={{ width: '50%', margin: 'auto' }}
+          FormHelperTextProps={{ style: { backgroundColor: 'rgb(238,238,238)', margin: '0' } }}
+          sx={{ background: 'white', width: '50%', margin: 'auto' }}
           id='outlined-basic'
           label='E-mail'
           multiline={true}
           value={formData.email}
+          error={emailError.status}
+          helperText={emailError.message}
           onChange={(event) => {
+            if (!isValidEmail(event.target.value))
+              setEmailError({ status: true, message: 'Invalid email' });
+            else setEmailError({ status: false, message: '' });
             setFormData({ ...formData, email: event.target.value });
           }}
         />
         <br />
         <br />
         <TextField
-          sx={{ width: '80%', margin: 'auto' }}
+          FormHelperTextProps={{ style: { backgroundColor: 'rgb(238,238,238)', margin: '0' } }}
+          sx={{ background: 'white', width: '80%', margin: 'auto' }}
           id='outlined-basic'
           label='Message'
           multiline={true}
           value={formData.message}
+          error={messageError.status}
+          helperText={messageError.message}
           onChange={(event) => {
+            if (event.target.value == '')
+              setMessageError({ status: true, message: 'Message cannot be empty' });
+            else setMessageError({ status: false, message: '' });
             setFormData({ ...formData, message: event.target.value });
           }}
         />
@@ -65,13 +101,18 @@ function Contact() {
           width: '-webkit-fill-available',
         }}
         onClick={() => {
-          sendEmail();
-          setFormData({ name: '', email: '', message: '' });
+          console.log(emailError);
+          if (!nameError.status && !emailError.status && !messageError.status) {
+            sendEmail();
+            setFormData({ name: '', email: '', message: '' });
+            handleClickOpen();
+          }
         }}
         variant='contained'
       >
         Send
       </Button>
+      <AlertDialog open={open} handleClose={handleClose} />
       <Footer />
     </div>
   );
---
+++ src/components/Dialog.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogTitle from '@mui/material/DialogTitle';
+
+function AlertDialog({ open, handleClose }: any) {
+  return (
+    <div>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby='alert-dialog-title'
+        aria-describedby='alert-dialog-description'
+      >
+        <DialogTitle id='alert-dialog-title'>{'Message sent successfully!'}</DialogTitle>
+
+        <DialogActions>
+          <Button onClick={handleClose} href='http://localhost:3000/' color='success'>
+            HOME PAGE
+          </Button>
+          <Button onClick={handleClose} color='error' autoFocus>
+            CLOSE
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
+
+export default AlertDialog;
